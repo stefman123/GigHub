f5f781556c5831841d8dc5d072e07d612f0a5f6f
stefan <stefangayle@hotmail.co.uk>
2016-07-29 14:35:18 +0100

    Implement anti-forgery token

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -31,6 +31,7 @@ namespace GigHub.Controllers
 
         [Authorize]
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Create(GigFormViewModel viewModel)
         {
             if (!ModelState.IsValid)
